2959c70893f4ccd862d6113d54d473a67d2c540d
yanglan201766 <32284086+yanglan201766@users.noreply.github.com>
2019-09-26 14:34:57 +0800

    Add files via upload

---
+++ Week4_课堂练习.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#import 库名
+import os
+os.path
+
+
+# In[9]:
+
+
+#import 库名, 库名
+import os, json
+json.__author__
+
+
+# In[11]:
+
+
+#import 库名 as 别名
+import numpy as np
+np.array
+
+
+# In[12]:
+
+
+#from 库名 import 函数
+from json import __author__
+__author__
+
+
+# In[13]:
+
+
+#from 库名 import 函数, 函数
+from json import __author__, decoder
+decoder
+
+
+# In[14]:
+
+
+#from 库名 import 函数 as 别名
+from json import decoder as dd
+dd
+
+
+# In[15]:
+
+
+#from 库名 import *
+from math import *
+pi
+
+
+# In[21]:
+
+
+#import 包名.子包.文件
+import ctypes.macholib.dyld
+ctypes.macholib.dyld.dylib_info
+
+
+# In[31]:
+
+
+#from 包名.子包 import 文件
+from ctypes.macholib import dyld
+dylib_info
+
+
+# In[30]:
+
+
+#form 包名.子包.文件 import 函数
+from ctypes.macholib.dyld import dylib_info
+dylib_info
+
+
+# In[ ]:
+
+
+
+
