c68f41d996bd6afea5d59d0ca77a30773360781a
yanglan201766 <32284086+yanglan201766@users.noreply.github.com>
2020-01-29 20:16:07 +0800

    Add files via upload

---
+++ Week6/Week6_课堂讲解.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import urllib
+
+
+# In[3]:
+
+
+import requests
+
+
+# In[4]:
+
+
+url = 'www.ftchinese.com/channel/economy.html'
+
+
+# In[ ]:
+
+
+
+
