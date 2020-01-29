09540d16eae3f273505ae824d5234db22b7a799d
yanglan201766 <32284086+yanglan201766@users.noreply.github.com>
2020-01-29 20:15:28 +0800

    Add files via upload

---
+++ Week4/Week4_课堂讲解.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import copy
+
+
+# In[2]:
+
+
+import sys
+
+
+# In[3]:
+
+
+print (sys.path)
+
+
+# In[4]:
+
+
+dir(copy)
+
+
+# In[15]:
+
+
+import numpy as np
+
+
+# In[16]:
+
+
+np.array
+
+
+# In[17]:
+
+
+import numpy
+numpy.array
+
+
+# In[8]:
+
+
+import math
+
+
+# In[9]:
+
+
+math.pi
+
+
+# In[10]:
+
+
+math.sin
+
+
+# In[11]:
+
+
+from math import pi
+
+
+# In[19]:
+
+
+pi
+
+
+# In[20]:
+
+
+sin
+
+
+# In[21]:
+
+
+import json.decode
+
+
+# In[22]:
+
+
+import json.decoder
+
+
+# In[23]:
+
+
+from json import decoder
+
+
+# In[24]:
+
+
+dir(decoder)
+
+
+# In[25]:
+
+
+import json
+
+
+# In[26]:
+
+
+dir(json)
+
+
+# In[27]:
+
+
+json.encoder
+
+
+# In[28]:
+
+
+json.loads
+
+
+# In[29]:
+
+
+import os
+
+
+# In[30]:
+
+
+print (os.getcwd())
+
+
+# In[31]:
+
+
+import sound.effects.echo
+
+
+# In[32]:
+
+
+from sound.effects import echo
+
+
+# In[ ]:
+
+
+
+
