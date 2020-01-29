075a5ce5cc6bc9722423e4a79bcbcf3a8459baf1
yanglan201766 <32284086+yanglan201766@users.noreply.github.com>
2020-01-29 20:22:12 +0800

    Add files via upload

---
+++ Week10/Week10_课堂讲解_类.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Forest():
+    """ Forest can grow trees which eventually die."""
+    
+
+
+# In[2]:
+
+
+import numpy as np
+np.info(Forest)
+
+
+# In[3]:
+
+
+forest = Forest()
+
+
+# In[ ]:
+
+
+
+
